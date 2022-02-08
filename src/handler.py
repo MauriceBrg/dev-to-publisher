6e1e1a99f9efba824a9b1d1d21cfad87e905f260
Maurice Borgmeier <mborgmeier@tecracer.de>
2022-02-08 16:16:34 +0100

    Fix some stuff and cleanup

--- src/handler.py
+++ src/handler.py
@@ -1,9 +1,15 @@
+"""
+Lambda function to publish dev.to articles.
+"""
 import json
 import os
 
 import requests
 
 def get_unpublished_articles(api_key):
+    """
+    Returns the unpublished articles for the api keys user.
+    """
 
     url = "https://dev.to/api/articles/me/unpublished"
 
@@ -16,6 +22,9 @@ def get_unpublished_articles(api_key):
     return response.json()
 
 def filter_list_of_articles(list_of_articles):
+    """
+    Reduces the amount of attributes in a list of articles.
+    """
 
     keep_keys = ["id", "title", "published", "published_at", "path", "url"]
 
@@ -25,30 +34,39 @@ def filter_list_of_articles(list_of_articles):
     ]
 
 def get_filtered_list_of_unpublished_articles(api_key):
+    """
+    Returns a list of unpublished articles with limited attributes.
+    """
 
     list_of_articles = get_unpublished_articles(api_key=api_key)
 
     return filter_list_of_articles(list_of_articles)
 
 def get_article_id_by_preview_url(preview_url, api_key):
-    
+    """
+    Returns the article id for a given preview_url.
+    """
+
     for article in get_filtered_list_of_unpublished_articles(api_key):
 
         # Preview URLs have a weird suffix, it is what it is...
         if article["url"] in preview_url:
             return article["id"]
-    
+
     return None
 
 def publish_article_by_id(article_id, api_key):
-    
+    """
+    Publish an article given its id and api_key
+    """
+
     url = f"https://dev.to/api/articles/{article_id}"
 
     print(f"Publishing article {article_id}")
 
-    response = requests.post(
+    response = requests.put(
         url=url,
-        data={
+        json={
             "article": {
                 "published": True
             }
@@ -60,9 +78,12 @@ def publish_article_by_id(article_id, api_key):
 
     assert response.json()["published_at"] is not None
 
-def lambda_handler(event, context):
+def lambda_handler(api_event, __unused):
+    """
+    Handles incoming requests from the API Gateway.
+    """
 
-    payload = json.loads(event["body"])
+    payload = json.loads(api_event["body"])
 
     assert "PostURL" in payload, "PostURL needs to be present"
     assert "ApiKey" in payload, "ApiKey needs to be present"
@@ -81,15 +102,19 @@ def lambda_handler(event, context):
 
     if article_id is not None:
         publish_article_by_id(article_id=article_id, api_key=api_key)
-    
+
     return {}
 
-if __name__ == "__main__":
-
+def test():
+    """Tests the lambda using the sample event"""
     path_to_sample_event = os.path.join(os.path.dirname(__file__), "sample_event.json")
-    with open(path_to_sample_event) as handle:
-        file_content = handle.read()
+    with open(path_to_sample_event, encoding="utf-8") as file_handle:
+        file_content = file_handle.read()
 
     event = json.loads(file_content)
 
     lambda_handler(event, {})
+
+if __name__ == "__main__":
+
+    test()
